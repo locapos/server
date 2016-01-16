ddec8a3ffaae93d41260885c6c578a07a631dbaf
yutaka <shigure@refy.net>
2016-01-16 21:10:37 +0900

    add auth providers

--- server.js
+++ server.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const app = express();
 
+const passport = require('passport')
+    , bodyParser = require('body-parser');
+
+const auth = require('./lib/auth');
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+auth.install(passport);
+
+// setup
+app.use(bodyParser.urlencoded({extended: true});
+app.use(require('express-session')({
+  secret: 'atashi',
+  resave: false,
+  saveUninitialized: false
+});
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/auth', auth);
+
+// handlers
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+app.user('/', express.static('./public'));
+
 app.get('/', function(req, res){
   res.send('ok');
 });
