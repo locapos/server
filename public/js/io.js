a8c2c4a16dd56ac4416912486c569a8d903f8c87
yutaka <shigure@refy.net>
2016-01-18 01:14:28 +0900

    add socket.io client

---
+++ public/js/io.js
@@ -0,0 +1,19 @@
+var markers = {};
+
+function update(obj){
+  var key = obj.provider + ':' + obj.id;
+  var opt = {
+    latlng: new google.maps.LatLng(obj.lat, obj.lon);
+  };
+  if(markers[key] === undefined){
+    markers[key] = new google.maps.Marker(opt);
+    markers[key].setMap(map);
+  }
+  markers[key].setOptions(opt);
+}
+
+var socket = io();
+socket.on('update', function(msg){
+  console.log(msg);
+  update(eval(msg));
+});
