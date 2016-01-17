31e89028416102c84fc1f2797b6e8fb91ba835bc
yutaka <shigure@refy.net>
2016-01-18 00:37:11 +0900

    fix typo

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
-var canvas = document.getElementById('map-canvas') ;
+var canvas = document.getElementById('map-canvas');
 
-var latlng = new google.maps.LatLng(35.792621, 139.806513 ;
+var latlng = new google.maps.LatLng(35.792621, 139.806513);
 
 var mapOptions = {
   zoom: 15,
