bb8556674512c37b6b18aa745690a855b576f847
yutaka <shigure@refy.net>
2016-01-18 02:32:06 +0900

    fix obj property

--- public/js/io.js
+++ public/js/io.js
@@ -3,7 +3,7 @@ var markers = {};
 function update(obj){
   var key = obj.provider + ':' + obj.id;
   var opt = {
-    latlng: new google.maps.LatLng(obj.latitude, obj.longitude)
+    position: new google.maps.LatLng(obj.latitude, obj.longitude)
   };
   if(markers[key] === undefined){
     markers[key] = new google.maps.Marker(opt);
