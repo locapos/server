2b02ed8ab09dcf9be0f8af58d985129564e1fca0
yutaka <shigure@refy.net>
2016-01-20 10:17:25 +0900

    remove maptype selector and street view control

--- public/js/map.js
+++ public/js/map.js
@@ -5,6 +5,8 @@ var latlng = new google.maps.LatLng(35.685825,139.754441);
 var mapOptions = {
   zoom: 9,
   center: latlng,
+  mapTypeControl: false,
+  streetViewControl: false,
 };
 
 var map = new google.maps.Map(canvas, mapOptions);
