170d1e2d4f3baa3032c291aafe8d24121d5ef170
yutaka <shigure@refy.net>
2016-03-01 10:56:29 +0900

    complient to OAuth2 Implicit grant

--- oauth/index.js
+++ oauth/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
-const crypto = require('crypto');
-
 const router = require('express').Router();
 
-const db = require('../lib/db.js')
+const hashgen = require('../lib/hashgen.js')
+    , db = require('../lib/db.js')
     , Q = require('q');
 
 router.get('/authorize', (req, res) => {
@@ -14,6 +13,7 @@ router.get('/authorize', (req, res) => {
   // manage session
   req.session.regenerate(() => {
     req.session.redirect_uri = req.query.redirect_uri;
+    req.session.state = req.query.state || '';
     res.render('oauth/authorize');
   });
 });
@@ -21,16 +21,16 @@ router.get('/authorize', (req, res) => {
 router.get('/redirect', (req, res) => {
   let uri = req.session.redirect_uri;
   if(!uri) return res.sendStatus(400);
-  let hash = crypto.createHash("sha256");
-  hash.update("" + Math.random() + Date.now());
-  let token = hash.digest('base64');
+  let token = hashgen();
+  let state = req.session.state || '';
   db.storeUser(token, req.user)
-    .then(v => Q.ninvoke(req.session, "destroy"))
-    .then(v => res.redirect(`${uri}?token=${encodeURIComponent(token)}`))
+    .then(v => Q.ninvoke(req.session, 'destroy'))
+    .then(v => res.redirect(`${uri}#access_token=${encodeURIComponent(token)}&token_type=bearer&state=${encodeURIComponent(state)}`))
     .catch(v => res.sendStatus(500)); // if got exception, send 500 err.
 });
 
 router.get('/failed', (req, res) => {
+  res.redirect('/authorize?error=1');
 });
 
 module.exports = router;
