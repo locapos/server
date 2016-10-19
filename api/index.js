8881ba330fb433b9a05854117fd58d6532aa4334
yutaka <shigure@refy.net>
2016-10-19 05:09:22 +0000

    staging api

--- api/index.js
+++ api/index.js
@@ -5,5 +5,8 @@ const router = require('express').Router();
 router.use('/locations', require('./locations.js'));
 router.use('/groups', require('./groups.js'));
 router.use('/users', require('./users.js'));
+if(process.env.LOCAPOS_STAGING){
+  router.use('/staging', require('./staging.js'));
+}
 
 module.exports = router;
---
+++ api/staging.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const router = require('express').Router();
+
+const hashgen = require('../lib/hashgen.js')
+    , db = require('../lib/db.js')
+    , SqlDb = require('../lib/sqldb.js')
+    , sdb = new SqlDb('./db/clients.sqlite3')
+    , Q = require('q')
+    , crypto = require('crypto');
+
+router.get('/gentoken', (req, res) => {
+  if(!req.query.client_id) return res.sendStatus(400);
+  if(!req.query.signature) return res.sendStatus(400);
+  if(!req.query.service) return res.sendStatus(400);
+  if(!req.query.screen_name) return res.sendStatus(400);
+  if(!req.query.id) return res.sendStatus(400);
+  let avatar = req.query.avatar || '';
+
+  sdb.get().then(d => d.all('SELECT * FROM secrets WHERE secret = ?', req.query.client_id))
+    .then(ids => ids.length == 0 ? (() => { throw 400; })() : Q(ids[0]))
+    .then(secret => {
+      let hmac = crypto.createHmac('sha256', secret.appsecret);
+      hmac.update(req.query.client_id + req.query.service + req.query.id);
+      let sig = hmac.digest('base64');
+      if(sig != req.query.signature){ throw 401; }
+      return Q({username: req.query.screen_name, default_username: req.query.screen_name,
+        id: req.query.id, provider: req.query.service, avatar: avatar});
+    })
+    .then(user => {
+      let prefix = hashgen.hmac(`${user.provider}:${user.id}`, process.env.CryptoHashKey);
+      let token = `${prefix}!${hashgen.hash()}`;
+      return db.searchUserToken(prefix)
+        .then(v => {
+          if(v.length == 0){
+            return db.storeUser(token, user);
+          }else{
+            let t = v[0].split(':')[1];
+            token = t;
+            return db.extendsExpire(t);
+          }
+        })
+        .then(_ => Q(token));
+    })
+    .then(token => res.send({key: token}))
+    .catch(e => res.sendStatus(typeof(e) === 'number' ? e : 500));
+});
+
+module.exports = router;
