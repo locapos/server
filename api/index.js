4e18eaf6cadfe5609b5a2db1ed5d22f735dd7427
yutaka <shigure@refy.net>
2016-10-26 18:31:48 +0900

    about locapos show in authorize page

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 router.use('/locations', require('./locations.js'));
 router.use('/groups', require('./groups.js'));
 router.use('/users', require('./users.js'));
+router.use('/terms', require('./terms.js'));
 if(process.env.LOCAPOS_STAGING){
   router.use('/staging', require('./staging.js'));
 }
