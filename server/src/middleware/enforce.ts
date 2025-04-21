dba4998a99a1e6193129736dc5e83786612993b3
yutaka tsumori <shigure@refy.net>
2025-04-21 13:03:07 +0000

    use Hono authorization parser

--- server/src/middleware/enforce.ts
+++ server/src/middleware/enforce.ts
@@ -1,46 +1,33 @@
 import { createMiddleware } from "hono/factory";
-import { HTTPException } from "hono/http-exception";
 import { AccessTokenRepository, User } from "../repositories/AccessTokenRepository";
+import { bearerAuth } from "hono/bearer-auth";
 
 type AuthUser = User & { hash: string; };
 
 export const enforce = createMiddleware<{
-  Bindings: Env,
+  Bindings: Env;
   Variables: {
     user: AuthUser;
-  }
-}>(async (c, next) => {
-  const authorization = c.req.header("Authorization");
-  const [type, token] = authorization?.split(" ") || [];
-  if (type !== "Bearer" || !token) {
-    throw new HTTPException(401);
-  }
-  // Validate token
-  const [prefix, hash] = token.split(".");
-  if (!prefix || !hash) {
-    throw new HTTPException(401);
-  }
-
-  const tokenRepository = new AccessTokenRepository(c.env.SDB);
-
-  // Cleanup expired tokens first
-  await tokenRepository.deleteExpired();
-
-  // Find token in database
-  const accessToken = await tokenRepository.getByToken(prefix, hash);
-  if (!accessToken) {
-    throw new HTTPException(401);
-  }
-
-  // Extend expiration
-  await tokenRepository.extendExpiration(prefix);
-
-  // Set user in context
-  c.set("user", {
-    ...tokenRepository.mapToUser(accessToken),
-    hash: prefix,
-  });
-
-  // Chain request
-  return next();
-});
+  };
+}>(bearerAuth({
+  async verifyToken(token, c) {
+    // Validate token
+    const [prefix, hash] = token.split(".");
+    if (!prefix || !hash) return false;
+    const tokenRepository = new AccessTokenRepository(c.env.SDB);
+    // Cleanup expired tokens first
+    await tokenRepository.deleteExpired();
+    // Find token in database
+    const accessToken = await tokenRepository.getByToken(prefix, hash);
+    if (!accessToken) return false;
+    // Extend expiration
+    await tokenRepository.extendExpiration(prefix);
+    // Set user in context
+    c.set("user", {
+      ...tokenRepository.mapToUser(accessToken),
+      hash: prefix
+    });
+    // Chain request
+    return true;
+  },
+}));
