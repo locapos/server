4b61e68f447d5bf4edf133de211fc43ccc4792b1
yutaka tsumori <shigure@refy.net>
2025-05-02 14:16:01 +0000

    restrict overlay layer in Japan region to reduce HTTP request and improve performance

--- client/src/nowcast-layer.ts
+++ client/src/nowcast-layer.ts
@@ -1,5 +1,10 @@
 import MapView from "./map-view";
 
+const LatitudeRange = [20, 47.5];
+const LongitudeRange = [120, 150];
+const BlankGif = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+const NowcastUrl = "https://www.jma.go.jp/bosai/jmatile/data/nowc/targetTimes_N1.json";
+
 export default class NowcastLayer {
   protected layer: NowcastImageMapType;
   private interval: number | null = null;
@@ -41,7 +46,7 @@ class NowcastImageMapType implements google.maps.MapType {
   private validtime: string = "";
 
   async fetchDate() {
-    const targetTimesN1 = await fetch("https://www.jma.go.jp/bosai/jmatile/data/nowc/targetTimes_N1.json");
+    const targetTimesN1 = await fetch(NowcastUrl);
     const targetTimesN1Json = await targetTimesN1.json<Array<{ basetime: string; validtime: string; }>>();
     const targetTimesN1Data = targetTimesN1Json[0];
     this.basetime = targetTimesN1Data.basetime;
@@ -50,28 +55,64 @@ class NowcastImageMapType implements google.maps.MapType {
 
   getTile(tileCoord: google.maps.Point | null, zoom: number): Element | null {
     if (tileCoord == null) return null;
-    if (zoom < 4) return null;
     // 利用可能なズームの中で一番近いものを選ぶ
     const available = [4, 6, 8, 10];
     const zNative = available.reduce((prev, curr) => Math.abs(curr - zoom) < Math.abs(prev - zoom) ? curr : prev);
-    if (zNative === zoom) {
-      const tile = document.createElement("img");
-      tile.src = this.#getTileUrl(tileCoord.x, tileCoord.y, zoom);
-      tile.style.opacity = "0.5";
-      return tile;
-    } else {
-      const tile = document.createElement("div");
+    // 一致するもしくは小さいレベルの画像を拡大して表示するパターン
+    if (zNative <= zoom) {
       const scale = 1 << (zoom - zNative);
       const factor = this.#scalingParameters(tileCoord, scale);
       const { x, y } = factor.coord;
       const { x: xScale, y: yScale } = factor.scale;
-      tile.style.backgroundImage = `url(${this.#getTileUrl(x, y, zNative)})`;
-      tile.style.backgroundRepeat = "no-repeat";
+      if (!this.#inRange(x, y, zNative)) {
+        return null;
+      }
+      // タイルの座標を計算
+      const tile = document.createElement("div");
       tile.style.width = "100%";
       tile.style.height = "100%";
+      tile.style.backgroundImage = `url(${this.#getTileUrl(x, y, zNative)})`;
       tile.style.backgroundSize = `${scale * 100}% ${scale * 100}%`;
       tile.style.backgroundPosition = `${xScale}% ${yScale}%`;
       tile.style.backgroundRepeat = "no-repeat";
+      tile.style.opacity = "0.5";
+      return tile;
+    }
+    // 大きいレベルの画像を複数枚使用してさらに縮小して表示するパターン
+    else {
+      const scaleInv = 1 << (zNative - zoom);
+      const scale = 1 / scaleInv;
+      const factor = this.#scalingParameters(tileCoord, scale);
+      const { x, y } = factor.coord;
+      const backgroundImages = [];
+      const backgroundSizes = [];
+      const backgroundPositions = [];
+      const backgroundRepeats = [];
+      const outOfRangeFlags = [];
+      for (let i = 0; i < scaleInv; i++) {
+        for (let j = 0; j < scaleInv; j++) {
+          if (!this.#inRange(x + i, y + j, zNative)) {
+            outOfRangeFlags.push(true);
+            backgroundImages.push(`url(${BlankGif})`);
+          } else {
+            backgroundImages.push(`url(${this.#getTileUrl(x + i, y + j, zNative)})`);
+          }
+          backgroundSizes.push(`${scale * 100}% ${scale * 100}%`);
+          backgroundPositions.push(`${(i / (scaleInv - 1) * 100)}% ${(j / (scaleInv - 1) * 100)}%`);
+          backgroundRepeats.push("no-repeat");
+        }
+      }
+      if (outOfRangeFlags.length === scaleInv * scaleInv) {
+        return null;
+      }
+      const tile = document.createElement("div");
+      tile.style.width = "100%";
+      tile.style.height = "100%";
+      tile.style.backgroundImage = backgroundImages.join(", ");
+      tile.style.backgroundSize = `${scale * 100}% ${scale * 100}%`;
+      tile.style.backgroundPosition = backgroundPositions.join(", ");
+      tile.style.backgroundSize = backgroundSizes.join(", ");
+      tile.style.backgroundRepeat = backgroundRepeats.join(", ");
       tile.style.opacity = "0.5";
       return tile;
     }
@@ -80,6 +121,7 @@ class NowcastImageMapType implements google.maps.MapType {
   releaseTile(): void { }
 
   #getTileUrl(x: number, y: number, z: number): string {
+    if (y < 0) return BlankGif;
     return `https://www.jma.go.jp/bosai/jmatile/data/nowc/${this.basetime}/none/${this.validtime}/surf/hrpns/${z}/${x}/${y}.png`
   }
 
@@ -99,6 +141,24 @@ class NowcastImageMapType implements google.maps.MapType {
       }
     }
 
+  }
+
+  // タイル座標とズームレベルからそのタイルが表示される範囲が事前に定義した緯度経度の範囲に収まるかどうかを判定する
+  #inRange(x: number, y: number, z: number): boolean {
+    // タイルの北西と南東の緯度経度を計算
+    const n = Math.PI - 2 * Math.PI * y / Math.pow(2, z);
+    const latNW = (180 / Math.PI) * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)));
+    const longNW = x / Math.pow(2, z) * 360 - 180;
+
+    const s = Math.PI - 2 * Math.PI * (y + 1) / Math.pow(2, z);
+    const latSE = (180 / Math.PI) * Math.atan(0.5 * (Math.exp(s) - Math.exp(-s)));
+    const longSE = (x + 1) / Math.pow(2, z) * 360 - 180;
+
+    // タイルの領域が定義された範囲と重なるかどうかをチェック
+    const latOverlap = !(latSE > LatitudeRange[1] || latNW < LatitudeRange[0]);
+    const longOverlap = !(longSE < LongitudeRange[0] || longNW > LongitudeRange[1]);
+
+    return latOverlap && longOverlap;
   }
 
   alt: string = "Nowcast";
